425720c4934005dddb771f3880de5f136093d528
Bulat <b.gabdullin@mail.ru>
2021-04-19 01:26:13 +0300

    my app

---
+++ Хочу помочь/SceneDelegate.swift
@@ -0,0 +1,56 @@
+//
+//  SceneDelegate.swift
+//  Хочу помочь
+//
+//  Created by Булат on 14.04.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let _ = (scene as? UIWindowScene) else { return }
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+
+           // controller identifier sets up in storyboard utilities
+           // panel (on the right), it called Storyboard ID
+        let viewController = storyboard.instantiateViewController(withIdentifier: "tabBarViewController") as? TabBarViewController
+        let mainVC = viewController?.viewControllers?[2]
+        viewController!.selectedViewController = mainVC
+
+           self.window?.rootViewController = viewController
+           self.window?.makeKeyAndVisible()
+    }
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+
+}
---
+++ Хочу помочь/TestViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TestViewController.swift
+//  Хочу помочь
+//
+//  Created by Булат on 17.04.2021.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
